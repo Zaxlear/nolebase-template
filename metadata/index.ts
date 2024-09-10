7cf0258893cfb323845867c3e1dc7b467cc1c453
Zaxlear <karl.kid1412@outlook.com>
2024-09-11 03:02:52 +0800

    update

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,6 +1,6 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
+export const siteName = 'Karl's Blog'
+export const siteShortName = 'Karl's Blog'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
